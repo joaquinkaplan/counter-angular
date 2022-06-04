53bb2e1436c59096ef2419727ccbebb1381c8e33
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-06-04 14:14:24 +0300

    counter app in angular TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'counterapp';
+
+  countNumber = 0;
+
+  resetBtn() {
+    this.countNumber = 0;
+  }
+
+  increaseBtn() {
+    this.countNumber += 1;
+  }
+
+  decreaseBtn() {
+    this.countNumber -= 1;
+  }
 }
